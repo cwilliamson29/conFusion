55697dc86a2585ac320919e26720e56e58e57f8d
Chris Williamson <chriswill0629@gmail.com>
2022-08-14 03:49:08 -0400

    fixing scss and JS

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $(".reserveButton").click(function() {
+    $("#reserveModal").modal('toggle');
+  });
+  $(".login").click(function() {
+    $("#loginModal").modal('toggle');
+  });
+})
+
+
+$('#mycarousel').carousel({
+  interval: 1000
+});
+
+$('#carouselButton').click(function() {
+  if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+
+    $('#mycarousel').carousel('pause');
+    $('#carouselButton').children('span').removeClass('fa-pause');
+    $('#carouselButton').children('span').addClass('fa-play');
+
+  } else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+
+    $('#mycarousel').carousel('cycle');
+    $('#carouselButton').children('span').removeClass('fa-play');
+    $('#carouselButton').children('span').addClass('fa-pause');
+  }
+
+});
